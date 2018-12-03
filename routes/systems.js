365ccdf6bc2e09ae1c4d57254e1373a0555e957f
Carolina Cordero <ccalvarez@gmail.com>
2018-12-03 15:59:46 -0600

    chore: agrega llamada a controllers desde routes

--- routes/systems.js
+++ routes/systems.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const systemsController = require('../controllers/systems');
+
 const router = express.Router();
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const usersController = require('../controllers/users');
+
 const router = express.Router();
 
 module.exports = router;
