8d8238cef7e23b188daebccb0a6bdd31b8fb69c7
Carolina Cordero <ccalvarez@gmail.com>
2018-12-05 16:16:24 -0600

    feat: agrega express-validator para validar email y password de nueva usuaria. #7

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,43 @@
 const express = require('express');
+const { body } = require('express-validator/check');
+const { sanitizeBody } = require('express-validator/filter');
+const { validationResult } = require('express-validator/check');
 
 const usersController = require('../controllers/users');
 
 const router = express.Router();
 
-router.post('/', (req, res, next) => {
-  usersController
-    .createUser(req.body.email, req.body.password)
-    .then(user => {
-      if (user != undefined) {
-        res.status(201).json(user);
-      }
-    })
-    .catch(err => res.status(500).json(`Se encontró un error: ${err}`));
-});
+router.post(
+  '/',
+  [
+    body('email')
+      .trim()
+      .isEmail()
+      .normalizeEmail(),
+    body('password')
+      .trim()
+      .isLength({ min: 5 }),
+  ],
+  (req, res, next) => {
+    // refactor: esto lo puedo enviar a controller, junto con req, res, next, para simplificar routes
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({
+        message: 'Validation failed, entered data is incorrect',
+        errors: errors.array(),
+      });
+    }
+
+    usersController
+      .createUser(req.body.email.trim(), req.body.password.trim())
+      .then(user => {
+        if (user != undefined) {
+          res.status(201).json(user);
+        }
+      })
+      .catch(err => res.status(500).json(`Se encontró un error: ${err}`));
+  }
+);
 
 router.get('/', (req, res, next) => {
   res.status(200).json({ prueba: 'prueba' });
