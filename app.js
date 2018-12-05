9830f960d59b3326678c5746bd9630cd44d3b23a
Carolina Cordero <ccalvarez@gmail.com>
2018-12-05 14:50:28 -0600

    feat: encripta el password al registrar nueva usuaria. #7

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://s.codepen.io');
   res.setHeader(
     'Access-Control-Allow-Methods',
     'GET, POST, PUT, PATCH, DELETE'
--- controllers/users.js
+++ controllers/users.js
@@ -1,19 +1,23 @@
+const bcrypt = require('bcryptjs');
+
 const UserModel = require('../models/user');
 
 exports.createUser = (email, password) => {
   return new Promise((resolve, reject) => {
     try {
-      const newUser = UserModel({
-        email: email,
-        password: password,
-      });
+      bcrypt.hash(password, 12).then(hashedPassword => {
+        const newUser = UserModel({
+          email: email,
+          password: hashedPassword,
+        });
 
-      newUser.save(err => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(newUser);
-        }
+        newUser.save(err => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(newUser);
+          }
+        });
       });
     } catch (ex) {
       reject(ex);
