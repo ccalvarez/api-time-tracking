737bc6fc8314e2ff829f5c6f3bdab90648a67a4d
Carolina Cordero <ccalvarez@gmail.com>
2018-12-19 14:47:11 -0600

    chore: cambia serverless-http de dependencia de desarrollo a dependencia de producción

--- express/server.js
+++ express/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const serverless = require('serverless-http');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
