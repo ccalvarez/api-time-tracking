22750f4d64af4ee2907a3a33f5768d91118c3e91
Carolina Cordero <ccalvarez@gmail.com>
2019-04-16 16:34:13 -0600

    feat

--- express/server.js
+++ express/server.js
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
   );
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
-  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   next();
 });
 
